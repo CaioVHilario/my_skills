d4564b6c9a9494c7abdbdd0a279c154bc28de546
CaioVHilario <caio.hilario@ufv.br>
2024-02-22 09:46:59 -0300

    Add TypeScript

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,13 +10,30 @@ import {
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
+interface SkillData {
+    id: string;
+    name: string;
+}
+
 export function Home() {
     const [newSkill, setNewSkill] = useState('');
-    const [mySkills, setMySkills] = useState([]);
+    const [mySkills, setMySkills] = useState<SkillData[]>([]);
     const [gretting, setGretting] = useState('');
 
     function handleAddNewSkill() {
-        setMySkills(oldState => [...oldState, newSkill]);
+        const data ={
+            id: String(new Date().getTime()),
+            name: newSkill
+        }
+
+        setMySkills(oldState => [...oldState, data]);
+
+    }
+
+    function handleRemoveSkill(id: string) {
+        setMySkills(oldState => oldState.filter(
+            skill => skill.id !== id
+        ))
 
     }
 
@@ -51,7 +68,10 @@ export function Home() {
             onChangeText={setNewSkill}
         />
 
-        <Button onPress={handleAddNewSkill} />
+        <Button 
+        onPress={handleAddNewSkill}
+        title='Add'
+        />
 
         <Text style={[styles.title, { marginVertical: 20 }]} >
             My SKills
@@ -59,9 +79,12 @@ export function Home() {
 
         <FlatList 
             data={mySkills}
-            keyExtractor={item => item}
+            keyExtractor={item => item.id}
             renderItem={({ item}) => (
-                <SkillCard skill={item}/>
+                <SkillCard 
+                    skill={item.name}
+                    onPress={() => handleRemoveSkill(item.id)}
+                />
             )}
         />
 
@@ -72,7 +95,7 @@ export function Home() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 20,
+        paddingHorizontal: 30,
         paddingVertical: 70,
 
         backgroundColor: '#121015',
